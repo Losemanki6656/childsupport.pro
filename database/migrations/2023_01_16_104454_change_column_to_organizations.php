25754b3605e8cf59b6e6ac4b71b35155fab3f2b1
Losemanki6656 <bbiznestg@gmail.com>
2023-01-16 15:48:02 +0500

    Asd

---
+++ database/migrations/2023_01_16_104454_change_column_to_organizations.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeColumnToOrganizations extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('organizations', function (Blueprint $table) {
+            $table->BigInteger('chat_reception_id')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('organizations', function (Blueprint $table) {
+            //
+        });
+    }
+}
